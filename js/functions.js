7b70d272e7ac84ec0a8f02ea74487bc9337b2d65
우엥 <102339975+Ueng123@users.noreply.github.com>
2022-06-25 18:17:04 +0900

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
